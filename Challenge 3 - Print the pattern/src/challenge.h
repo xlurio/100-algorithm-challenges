220e2db94b3c72609ca89e26cb323c7503558064
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-09-16 16:21:15 -0300

    dsa: challenge 3 started

--- Challenge 3 - Print the pattern/src/challenge.h
+++ Challenge 3 - Print the pattern/src/challenge.h
@@ -1,18 +1,31 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-char *print_pattern(int pattern_length)
+char *get_line(int pattern_length);
+char *get_number(int number, int pattern_length);
+
+char *get_pattern(int pattern_length)
 {
+    for (int i = pattern_length; i > 0; i++)
+    {
+        get_line(i);
+    }
 }
 
-char *print_line(int pattern_length)
+char *get_line(int pattern_length)
 {
-    int i, j;
+    int i;
 
     for (int i = pattern_length; i > 0; i--)
     {
-        for (int j = pattern_length; j > 0; j--)
-        {
-            printf("%i", i);
-        }
+        get_number(i, pattern_length);
+    }
+}
+
+char *get_number(int number, int pattern_length)
+{
+    for (int j = pattern_length; j > 0; j--)
+    {
+        printf("%i", number);
     }
 }
