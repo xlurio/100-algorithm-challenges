69406be56179ffef0b053d4aa2d3cea293d466a9
Lucas Calegario <lucas.c.calegario@outlook.com>
2022-10-04 22:26:51 -0300

    dsa: trying challenge 5

--- Challenge 5 - Triplets with zero sum/src/challenge.h
+++ Challenge 5 - Triplets with zero sum/src/challenge.h
@@ -1,1 +1,45 @@
-int count_triplets(int *array_to_check);
+int check_for_third_value(
+  int value_to_find, int *array_to_search, int first_index, int size
+);
+
+int count_triplets(int *array_to_check, int size) {
+  int first_value, second_value, value_to_find, is_there_triple_result_zero = 0;
+  int number_of_triplets = 0;
+  int i, j;
+
+  for (i = 0; i < size; i++) {
+    first_value = array_to_check[i];
+
+    for (j = i + 1; j < size; j++) {
+      second_value = array_to_check[j];
+
+      value_to_find = - first_value - second_value;
+
+      is_there_triple_result_zero = check_for_third_value(
+        value_to_find, array_to_check, j + 1, size
+      );
+
+      if (is_there_triple_result_zero) {
+        number_of_triplets++;
+      }
+    }
+  }
+
+  return number_of_triplets;
+}
+
+int check_for_third_value(
+  int value_to_find, int *array_to_search, int first_index, int size
+) {
+  int current_index = 0;
+
+  for (int i = first_index; i < size; i++) {
+    current_index = array_to_search[i];
+
+    if (current_index == value_to_find) {
+      return 1;
+    }
+  }
+
+  return 0;
+}
--- Challenge 5 - Triplets with zero sum/tests/test_challenge.cc
+++ Challenge 5 - Triplets with zero sum/tests/test_challenge.cc
@@ -9,7 +9,7 @@ TEST(ChallengeTests, test_case1)
 {
   int array_for_test[] = {0, -1, 2, -3, 1};
   int expected_result = 1;
-  int actual_result = count_triplets(array_for_test);
+  int actual_result = count_triplets(array_for_test, 5);
 
   EXPECT_EQ(expected_result, actual_result);
 }
@@ -18,7 +18,7 @@ TEST(ChallengeTests, test_case2)
 {
   int array_for_test[] = {1, 2, 3};
   int expected_result = 0;
-  int actual_result = count_triplets(array_for_test);
+  int actual_result = count_triplets(array_for_test, 3);
 
   EXPECT_EQ(expected_result, actual_result);
 }
